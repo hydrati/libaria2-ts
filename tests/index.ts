d1a06fd720a5c3f215378211edb558582d8470e9
Hydrogen <3038094028@qq.com>
2021-12-05 01:18:10 +0800

    fix: test

--- tests/index.ts
+++ tests/index.ts
@@ -7,12 +7,13 @@ import { WebSocket as Aria2WebSocket } from "../src/lib";
     port: 6800,
   });
 
-  const gid1 = await client1.addUri(
-    "https://sharepoint.latest10.win/19042.804/19042.804.210131-1457.VB_RELEASE_SVC_PROD1_CLIENTMULTI_X64FRE_ZH-CN.ISO"
-  );
+  // const gid1 = await client1.addUri(
+  //   "https://sharepoint.latest10.win/19042.804/19042.804.210131-1457.VB_RELEASE_SVC_PROD1_CLIENTMULTI_X64FRE_ZH-CN.ISO"
+  // );
   console.log("added", gid1);
-  client1.onceDownloadStart().then((event) => {
-    console.log("dl started,", this, event);
-  });
-  client1.closeConnection();
+  console.log(await client1.getVersion())
+  client1.on('aria2.onDownloadStart', ({gid}) => {
+     console.log(gid)
+  })
+  client1.close();
 })();
